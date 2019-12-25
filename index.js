d8fab8b73bdc8e581b4a0f80ff074020b50bf3c1
Sahithi-99 <sahithinarina99@gmail.com>
2019-12-25 16:10:47 +0530

    ggf

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ const app = express()
 const port = process.env.PORT||3000
 app.use(express.static('public'))
 app.get('/', (req, res) => res.sendFile(__dirname+'/public/html/index.html'))
+var obj={}
+obj.name = "Sahithi";
+obj.college = "CMRCET";
+obj.regno = "17H51A05F7";
 
+app.get('/data', (req, res) => res.json(obj))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
